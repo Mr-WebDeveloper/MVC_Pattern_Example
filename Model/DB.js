1dd1fb1d4afff7eebaaa67ed210d7d3a932157e8
Mr-WebDeveloper <mrmuthukamatchi@gmail.com>
2024-06-30 11:34:52 +0530

    MVC Pattern Example

--- Model/DB.js
+++ Model/DB.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const connectDatabase = () => {
-    return mongoose.connect('mongodb+srv://muthu:Mkmk2020@cluster0.koo7gtc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+    return mongoose.connect('mongodb://localhost:27017/')
     .then(() => {
         console.log("Database Created Successfully");
     })
